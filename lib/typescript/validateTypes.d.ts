d6f030b480827f61a1527c1d2c365713318501b3
Ramez Rafla <ramez@classroomapp.com>
2025-01-20 17:14:47 -0500

    removed typescript folder

--- lib/typescript/validateTypes.d.ts
+++
@@ -1,3 +0,0 @@
-import type { DocumentPickerResponse } from './types';
-export declare const safeValidate: (requestedTypes: string[], singlePickResult: Pick<DocumentPickerResponse, "nativeType">) => boolean;
-//# sourceMappingURL=validateTypes.d.ts.map
